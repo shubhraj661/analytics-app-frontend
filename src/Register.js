efff78b68c5af1c461e4860127d1586047a1768f
Gaurav Bansal <urrgb29@gmail.com>
2024-07-09 21:24:01 +0530

    small fix url

--- src/Register.js
+++ src/Register.js
@@ -18,7 +18,7 @@ const Register = ({logIn}) => {
     }
 
     try {
-      const response = await axios.post('${process.env.REACT_APP_BACKEND_URL}/api/register', {
+      const response = await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/register`, {
         username,
         email,
         password,
